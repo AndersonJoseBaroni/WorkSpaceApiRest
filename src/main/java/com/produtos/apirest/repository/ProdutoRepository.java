28f363c25f6faa5323d026c18c99911e2aa95f4a
Anderson <anderson.baroni@gmail.com>
2019-06-01 20:31:11 -0300

    Adiconado comentario na Classe Repositorio Produto

--- src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
+++ src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.produtos.apirest.models.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+    // Consulta apenas um produto por id
     Produto findById(long id);
 }
